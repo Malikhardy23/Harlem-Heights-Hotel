f84aba5699c73c05a728c05b783266594ed3dabb
Malikhardy23 <50418576+Malikhardy23@users.noreply.github.com>
2020-09-27 00:14:22 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,71 +1,34 @@
-// SLIDESHOW //
+let sliderImages = document.querySelectorAll('.image');
+let arrowLeft = document.getElementById("backBtn");
+let dot = document.getElementsByClassName('.fa-circle')
+let arrowRight = document.getElementById("forwardBtn");
 
-let slideIndex = 0;
+var slideIndex = 1;
+showSlides(slideIndex);
 
-let arrows = document.getElementById("arrows");
-
-// Buttons //
-
-const leftBtn = document.getElementById("backBtn");
-
-const rightBtn = document.getElementById("forwardBtn");
-
-// NEXT & PREVIOUS CONTROLS //
-
-const plusSlides = (n) => {
-    showSlides(slideIndex += n)
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-// THUMBNAIL IMAGE CONTROLS //
-
-const currentSlide = (n) => {
-    showSlides(slideIndex = n)
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-const showSlides = (n) => {
-    let i;
-    let slides = document.querySelectorAll('.image');
-    let dots = document.getElementsByClassName('fa fa-circle');
-
-    if(n > slides.length){slideIndex = n};
-
-    if(n < 1){slideIndex = slides.length}
-
-    for(i = 0; i < slides.length; i++){
-        slides[i].style.display = "none";
-    }
-
-    for(i = 0; i < dots.length; i++){
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-
-    //for(i = 0; i < slides.length; i++){
-    //    slides[i].style.display = "none";
-    //}
-    //slideIndex++;
-    //if (slideIndex > slides.length) {slideIndex = 1}
-    //slides[slideIndex-1].style.display = "block";
-    // setTimeout(showSlides, 6000); // Change image every 6 seconds
-
-    slides[slideIndex-1].style.display = "block";
-
-    dots[slideIndex-1].className += " active";
+function showSlides(n) {
+  var i;
+  var slides = document.querySelectorAll('.image');
+  var dots = document.getElementsByClassName('fa-circle')
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
 
-plusSlides();
-currentSlide();
-showSlides(n);
-
-
-    // NEXT BUTTON //
-    //rightBtn.addEventListener('click', (e) => {
-    //    if(e.target.value === rightBtn){
-    //    }
-    // })
-    
-
-    // BACK BUTTON //
-    //  leftBtn.addEventListener('click', (e) => {
-    //    if(e.target.value === leftBtn){
-    //  }
-    //})
